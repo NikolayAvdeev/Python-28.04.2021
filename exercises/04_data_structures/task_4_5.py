f53b154f7c8c27115bd086b4b7a1f85ee0367b8a
NikolayAvdeev <ciscoccnarouter@gmail.com>
2021-04-30 01:54:41 -0500

    Решено 4.5

--- exercises/04_data_structures/task_4_5.py
+++ exercises/04_data_structures/task_4_5.py
@@ -22,3 +22,10 @@
 
 command1 = "switchport trunk allowed vlan 1,2,3,5,8"
 command2 = "switchport trunk allowed vlan 1,3,8,9"
+
+
+comm1 = set(command1.split()[-1].split(',')) 
+comm2 = set(command2.split()[-1].split(',')) 
+
+result = sorted(comm1 & comm2)
+print(result)
